5958362d1fd8fd4df74e4d6575df3c8e346cd951
johits <74091087+johits@users.noreply.github.com>
2021-09-23 17:46:05 +0900

    수정

--- app/src/main/java/com/example/screentest/MainActivity.kt
+++ app/src/main/java/com/example/screentest/MainActivity.kt
@@ -19,26 +19,33 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val display = windowManager.defaultDisplay // in case of Activity
+/* val display = activity!!.windowManaver.defaultDisplay */ // in case of Fragment
         val size = Point()
         display.getRealSize(size) // or getSize(size)
-        val width = size.x
-        val height = size.y
+        val widthx = size.x
+        val heighty = size.y
         val name = Build.MODEL
         val dm = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(dm)
+        val width = dm.widthPixels.coerceAtMost(dm.heightPixels)
 
         //인치 구하기
         val x = Math.pow((dm.widthPixels / dm.xdpi).toDouble(), 2.0)
         val y = Math.pow((dm.heightPixels / dm.ydpi).toDouble(), 2.0)
         val screenInches = sqrt(x + y)
 
+
+
         binding.apply {
             tvName.text = "모델명: $name"
-            tvInch.text = "인치: $screenInches"
-            tvX.text = "가로: $width"
-            tvY.text = "세로: $height"
-            tvDpi.text = "DPI: ${dm.densityDpi}"
-            tvSwResult.text = "sw dp: ${width / (dm.densityDpi / 160.0)}"
+            tvInch.text = "인치: ${String.format("%.1f", screenInches)} inch"
+            tvX.text = "가로: $widthx"
+            tvY.text = "높이: $heighty"
+            tvRealY.text = "실제 사용 높이: ${dm.heightPixels}"
+            tvDpi.text = "DPI: ${dm.densityDpi} dpi"
+            tvSw.text = "smallest Width: $width"
+            tvSwResult.text =
+                "Smallest Width DP: ${String.format("%.1f", width / (dm.densityDpi / 160.0))}"
         }
 
     }
