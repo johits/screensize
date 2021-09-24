56c43779364222cf77653f4031edead9a43543f4
johits <74091087+johits@users.noreply.github.com>
2021-09-24 16:52:27 +0900

    수정

--- app/src/main/java/com/example/screentest/MainActivity.kt
+++ app/src/main/java/com/example/screentest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Point
 import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.screentest.databinding.ActivityMainBinding
@@ -28,20 +29,29 @@ class MainActivity : AppCompatActivity() {
         val name = Build.MODEL
         val dm = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(dm)
-        val width = dm.widthPixels.coerceAtMost(dm.heightPixels)
+//        val width = dm.widthPixels.coerceAtMost(dm.heightPixels) 원래코드
+        val width = widthx.coerceAtMost(heighty)
 
         //인치 구하기
         val x = Math.pow((dm.widthPixels / dm.xdpi).toDouble(), 2.0)
         val y = Math.pow((dm.heightPixels / dm.ydpi).toDouble(), 2.0)
         val screenInches = sqrt(x + y)
 
+        val displayMetrics = this.resources.displayMetrics
+        val dpHeight = displayMetrics.heightPixels / displayMetrics.density
+        val dpWidth = displayMetrics.widthPixels / displayMetrics.density
+        Log.e("dpHeight-----", dpHeight.toString())
+        Log.e("dpWidth------", dpWidth.toString())
+        Log.e("ddddddd---", displayMetrics.density.toString())
+        Log.e("ddddsssddd---", displayMetrics.heightPixels.toString())
+        Log.e("dddddddasdasdasddddd---", displayMetrics.widthPixels.toString())
 
 
         binding.apply {
             tvName.text = "모델명: $name"
             tvInch.text = "인치: ${String.format("%.1f", screenInches)} inch"
-            tvX.text = "가로: $widthx"
-            tvY.text = "높이: $heighty"
+            tvX.text = "가로: $widthx px (${String.format("%.1f",dpWidth)} dp)"
+            tvY.text = "높이: $heighty px (${String.format("%.1f",dpHeight)} dp)"
             tvRealY.text = "실제 사용 높이: ${dm.heightPixels}"
             tvDpi.text = "DPI: ${dm.densityDpi} dpi"
             tvSw.text = "smallest Width: $width"
@@ -51,7 +61,6 @@ class MainActivity : AppCompatActivity() {
         binding.tvName.setOnClickListener {
             val intent = Intent(this,MainActivity2::class.java)
             startActivity(intent)
-            finish()
         }
 
     }
